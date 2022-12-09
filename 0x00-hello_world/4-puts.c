54549b3ecd83c95192fd66e55622a7918c976664
adudenamdmanny <emmanueljames.essien@gmail.com>
2022-12-08 21:42:41 -0800

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,6 +14,5 @@ int main(void)
 
 	        puts("\"Programming is like building a multi lingual puzzle);                                                            
 
-			        return (0);
-
+			        return": (0);
 }
