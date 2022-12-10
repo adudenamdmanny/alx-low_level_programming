62918bc9ee319741a95b7a94c03a246ef3e170b5
adudenamdmanny <emmanueljames.essien@gmail.com>
2022-12-09 21:13:10 -0800

    Hot Fixes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,9 +11,9 @@ int main(void)
 		int n;
 
 		for (n = 0; n < 10; n++)
-			putchar((num % 10) + '0')
-				
-				putchar('\n');
-		
+			putchar((n % 10) + '0');
+
+		putchar('\n');
+
 		return (0);
 }
