5417bb03ecdb9e03350bf39b78aa30621547ee3b
adudenamdmanny <emmanueljames.essien@gmail.com>
2022-12-19 12:53:46 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
- * reset_to_98 - update the value of n to 98
- * @n: input
- * Return: n
+ * reset_to_98 - Resets the value of a variable back to 98
+ * @n:Variable to be reset.
+ * Return:Void.
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
